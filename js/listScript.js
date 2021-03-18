266f6cfdf1c2490ab49dfe18e549adf38f625713
David Tavares <davidtavaresfs@gmail.com>
2021-03-18 09:09:19 -0300

    Implementando filter by holes

--- js/listScript.js
+++ js/listScript.js
@@ -1,12 +1,19 @@
 let conteudoTabela = document.getElementById("conteudoTabela");
+let isRender;
 let employees = [];
 let roles = [];
 let sortSelect = document.getElementById("sort");
+var check = document.querySelectorAll("input");
 
 async function init() {
   [employees, roles] = await Promise.all([listEmployees(), listRoles()]);
   employees.sort(arraySortNameAscending);
   sortSelect.addEventListener("change", () => selectedSort());
+
+  for (var i = 0; i < check.length; i++) {
+    check[i].addEventListener("click", updateTable);
+  }
+
   renderTable();
 }
 
@@ -28,6 +35,40 @@ function renderTable() {
     row.appendChild(cellRole);
     row.appendChild(cellSalary);
     conteudoTabela.appendChild(row);
+    isRender = true;
+  }
+}
+
+function updateTable() {
+  conteudoTabela.innerHTML = "";
+  let isAnyChecked = false;
+  for (var i = 0; i < check.length; i++) {
+    if (check[i].checked == true) {
+      isAnyChecked = true;
+      let checkedId = check[i].id;
+      for (const employee of employees) {
+        let role = roles.find((role) => role.id == employee.role_id);
+        if (role.id == checkedId) {
+          const row = document.createElement("tr");
+          const cellId = document.createElement("td");
+          const cellName = document.createElement("td");
+          const cellRole = document.createElement("td");
+          const cellSalary = document.createElement("td");
+          cellId.textContent = employee.id;
+          cellName.textContent = employee.name;
+          cellRole.textContent = role.name;
+          cellSalary.textContent = employee.salary;
+          row.appendChild(cellId);
+          row.appendChild(cellName);
+          row.appendChild(cellRole);
+          row.appendChild(cellSalary);
+          conteudoTabela.appendChild(row);
+        }
+      }
+    }
+  }
+  if (!isAnyChecked) {
+    renderTable();
   }
 }
 
@@ -41,7 +82,7 @@ function selectedSort() {
   } else if (sortSelect.value == 4) {
     employees.sort(arraySortSalaryDownward);
   }
-  renderTable();
+  updateTable();
 }
 
 function arraySortNameAscending(a, b) {
